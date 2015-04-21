6270db6de02ccd569930ccea8621eeb2d526c598
Bryan Borgeson & Jonathan Lin <student@epicodus.com>
2015-04-21 14:21:27 -0700

    Add adoptability functionality:

--- controllers/AnimalCtrl.js
+++ controllers/AnimalCtrl.js
@@ -1,10 +1,17 @@
 AnimalShelter.controller("AnimalCtrl", function AnimalCtrl($scope) {
     $scope.animals = [];
+    $scope.adoptedAnimals = [];
 
     $scope.addAnimal = function() {
         $scope.animals.push({ name: $scope.animalName, type: $scope.animalType });
         $scope.animalName = null;
         $scope.animalType = "";
+    }
+
+    $scope.adoptAnimal = function(animal) {
+        var index = $scope.animals.indexOf(animal);
+        $scope.animals.splice(index, 1);
+        $scope.adoptedAnimals.push(animal);
     }
 
 
