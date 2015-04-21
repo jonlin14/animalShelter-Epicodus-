76093a390a3f674e727bf6f56c4985fcf5ccc647
Bryan Borgeson & Jonathan Lin <student@epicodus.com>
2015-04-21 14:08:34 -0700

    Add animal type select

--- controllers/AnimalCtrl.js
+++ controllers/AnimalCtrl.js
@@ -2,8 +2,9 @@ AnimalShelter.controller("AnimalCtrl", function AnimalCtrl($scope) {
     $scope.animals = [];
 
     $scope.addAnimal = function() {
-        $scope.animals.push({ name: $scope.animalName });
+        $scope.animals.push({ name: $scope.animalName, type: $scope.animalType });
         $scope.animalName = null;
+        $scope.animalType = "";
     }
 
 
